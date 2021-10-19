3939f1462e560f702444df519780993020dfe3dd
NicholasHorta <nickdeveloper.js@gmil.com>
2021-10-19 19:21:22 +0100

    Multi level FOR LOOP working

--- src/app/services/match-setup.service.ts
+++ src/app/services/match-setup.service.ts
@@ -8,11 +8,11 @@ import playersListJSON from '../../assets/league_players.json'
 
 export class MatchSetupService {
 
-
   leagueTypeValue: number = 2;
   leagueTeamsArray: string[] = [
     'Iberia FC', 'White Isles Utd', 'Bavaria Stark', 'Clube Europa'
   ];
+
   generatedTeamsArray: string[] = [];
 
   matchTypeHandler(matchType: number) {
@@ -20,18 +20,51 @@ export class MatchSetupService {
     return this.leagueTypeValue
   }
 
+  // A copy of the teams array is taken to remove two teams randomly 
+  // The result is returned in an array of two remaining teams
   quickMatchSetupHandler() {
     let quickMatchTeamsArray: string[] = [...this.leagueTeamsArray];
-    for(let i = 0; i < this.leagueTypeValue; i++){
+    for (let i = 0; i < this.leagueTypeValue; i++) {
       quickMatchTeamsArray.splice(
         Math.floor(Math.random() * quickMatchTeamsArray.length), 1);
     }
+    this.generateTeamLists(this.leagueTypeValue);
     return this.generatedTeamsArray = [...quickMatchTeamsArray];
   }
 
+  // The result is returned in an array of all teams
   leagueSetupHandler() {
+    this.generateTeamLists(this.leagueTypeValue);
     return this.generatedTeamsArray = [...this.leagueTeamsArray];
   }
+
+  generateTeamLists(teamsAmount: number) {
+    const keeper = [...playersListJSON['keeper']];
+    const defence = [...playersListJSON['def']];
+    const defensiveMid = [...playersListJSON['def-mid']];
+    const attackingMid = [...playersListJSON['att-mid']];
+    const attack = [...playersListJSON['att']];
+    const asd = [keeper, defence, defensiveMid, attackingMid, attack];
+    let arr: Object[] = [];
+    const teamSheetPlayerAmt: number[] = [1, 5, 4, 3, 2];
+    
+    for (let x = 0; x < teamsAmount; x++) {
+      for (let i = 0; i < teamSheetPlayerAmt.length; i++) {
+        for (let j = 0; j < teamSheetPlayerAmt[i]; j++) {
+          arr.push(asd[i][j])
+        }
+      }
+    }
+    console.log('%cmatch-setup.service.ts line:58 arr', 'color: #007acc;', arr);
+
+    // this.genKeepers();
+  }
+  // const keeper = { posts: [...playersListJSON['keeper']]};
+  // const defence = { def: [...playersListJSON['def']]};
+  // const defensiveMid = { defMid: [...playersListJSON['def-mid']]}; 
+  // const attackingMid = { attMid: [...playersListJSON['att-mid']]};
+  // const attack = { att: [...playersListJSON['att']]};
+
 
 
 }
