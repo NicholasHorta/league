b87dbf3f572b92af73c083d51f8dacb9aac3231f
NicholasHorta <nickdeveloper.js@gmil.com>
2022-02-26 18:43:29 +0000

    init

--- src/app/my-directive.directive.ts
+++ src/app/my-directive.directive.ts
@@ -5,20 +5,20 @@ import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@an
 })
 export class MyDirectiveDirective {
 
-  // constructor(private item: ElementRef, private render: Renderer2) { }
+  constructor(private item: ElementRef, private render: Renderer2) { }
 
-  // ngOnInit(){
-  //   console.log('%cmy-directive.directive.ts line:11 "WORKING YOU FAG!"', 'color: #007acc;', "WORKING YOU FAG!");
-  // }
+  ngOnInit(){
+    console.log('%cmy-directive.directive.ts line:11 "WORKING YOU FAG!"', 'color: #007acc;', "WORKING YOU FAG!");
+  }
 
-  // @HostBinding() over: boolean = false;
+  @HostBinding() over: boolean = false;
 
-  // @HostListener('mouseover') click(eventData: Event){
-  //   console.log('%cmy-directive.directive.ts line:17 eventData', 'color: #007acc;', eventData);
-  // }
-  // @HostListener('mouseleave') mouseleave(eventData: Event){
-  //   this.over = false
-  // }
+  @HostListener('mouseover') click(eventData: Event){
+    console.log('%cmy-directive.directive.ts line:17 eventData', 'color: #007acc;', eventData);
+  }
+  @HostListener('mouseleave') mouseleave(eventData: Event){
+    this.over = false
+  }
 
 
 }
