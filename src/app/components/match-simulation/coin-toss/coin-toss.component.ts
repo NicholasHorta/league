da4e71f24760d7fe1e702f0f71ae97f1d1e05701
NicholasHorta <nickdeveloper.js@gmil.com>
2022-03-04 23:49:59 +0000

    Struc directive viewing

--- src/app/components/match-simulation/coin-toss/coin-toss.component.ts
+++ src/app/components/match-simulation/coin-toss/coin-toss.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-coin-toss',
+  selector: 'coin-toss',
   templateUrl: './coin-toss.component.html',
   styleUrls: ['./coin-toss.component.css']
 })
--- src/app/components/match-simulation/coin-toss/coin-toss.directive.ts
+++ src/app/components/match-simulation/coin-toss/coin-toss.directive.ts
@@ -1,13 +1,26 @@
-import { Directive, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Directive, Input, OnInit, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
 import { MatchSetupService } from 'src/app/services/match-setup.service';
+
+class AdditionalProperties {
+  constructor(private matchSetupSVC: MatchSetupService){}
+  public availableTeams = this.matchSetupSVC.generatedTeamsArray;
+}
 @Directive({
-  selector: '[CoinToss]'
+  selector: '[coinToss]'
 })
-export class CoinTossDirective {
 
-  constructor(private matchSetupSVC: MatchSetupService, private viewContRef: ViewContainerRef, private templateRef: TemplateRef<HTMLDivElement>) { }
 
-  
+export class CoinTossDirective implements OnInit {
+
+  constructor(private matchSetupSVC: MatchSetupService,private viewContRef: ViewContainerRef, private templateRef: TemplateRef<any>) { }
+  @Input('coinToss') coinTossImplemented = true; 
+  private props = new AdditionalProperties(this.matchSetupSVC);
+
+  ngOnInit(){
+    
+    this.viewContRef.createEmbeddedView(this.templateRef, this.props);
+
+  }
 
 
 }
